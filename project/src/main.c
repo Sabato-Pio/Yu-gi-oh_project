6e4b8aa13c1c7398e2cf44c257ed535be69a8798
Sabato Pio <sianosabatopio@gmail.com>
2026-06-06 13:09:58 +0200

    [GRAFICA] Migliorata grafica del terminale
    Aggiunto colore al messaggio di benvenuto e reso il terminale più pulito

--- project/src/main.c
+++ project/src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "item.h"
 #include "carta.h"
 #include "btree.h"
 
 int main() {
-    printf("CIAO! Benvenuto nel Database Tier di Yugioh Meta!\n\n");
+    system("cls");
+    printf("\033[34mCIAO! Benvenuto nel Database Tier di Yugioh Meta!\033[0m\n\n"); 
     carta c1 = creaCarta(68464358, "Caduta di Albaz", "Albaz", 1800, 0);
     carta c2 = creaCarta(25801745, "Kitkallos Tearlaments", "Tearlaments", 2300, 1200);
 
